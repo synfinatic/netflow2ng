f4f20c3f963963c076bc707d836d469c236e01f5
Kelly Byrd <kbyrd@memcpy.com>
2025-08-18 19:52:08 +0000

    Add an option to output in ntop's TLV format.
    As of ntop v6.4, the JSON format is no longer accepted for
    community mode: https://github.com/synfinatic/netflow2ng/issues/89
    
    This commit adds an --tlv commandline parameter that will be
    accepted by ntopng as of v6.4
    
    The format is "interesting" so I put it all in its own file. See
    that file for details on the format.
    
    TESTING:
    * Running netflow2ng on Debian 13, router sending it Netflow v9
    * ntopng v6.4 (installed from packages) on different host.
    * Confirmed ntop was in community mode and
      that I could see recent flows that matched my network activity.
    
    FUTURE WORK:
    * Test against top-of-tree ntopng
    * Update the ZmqHeader struct to the most recent one in the ntopng
      code, in an effort to stay ahead of any more deprecation.

--- cmd/zmq.go
+++ cmd/zmq.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/cloudflare/goflow/v3/decoders/netflow"
 	flowmessage "github.com/cloudflare/goflow/v3/pb"
+
 	// nolint SA1019 the new google.golang.org/protobuf/proto package is not backwards compatible
 	"github.com/golang/protobuf/proto"
 	zmq "github.com/pebbe/zmq4"
@@ -46,6 +47,8 @@ func StartZmqProducer() (*ZmqState, error) {
 	serialize := "json"
 	if rctx.cli.Protobuf {
 		serialize = "pbuf"
+	} else if rctx.cli.TLV {
+		serialize = "tlv"
 	}
 
 	log.Infof("Started ZMQ listener on: %s", rctx.cli.ListenZmq)
@@ -66,11 +69,13 @@ func StartZmqProducer() (*ZmqState, error) {
  * include/ntop_typedefs.h, include/ntop_defines.h & src/ZMQCollectorInterface.cpp from
  * https://github.com/ntop/ntopng
  */
-const ZMQ_MSG_VERSION = 2 // ntopng message version 2
-const ZMQ_TOPIC = "flow"  // ntopng only really cares about the first character!
+const ZMQ_MSG_VERSION_OLD = 2 // ntopng message version 2 for ntopng less than v6.4
+const ZMQ_MSG_VERSION_TLV = 3 // ntop message version for TLV for ntopng v6.4 and later.
+const ZMQ_TOPIC = "flow"      // ntopng only really cares about the first character!
 
 var MessageId uint32 = 0 // Every ZMQ message we send should have a uniq ID
 
+// This is zmq_msg_hdr_v1 from ntop_typedefs.h May want to update to zmq_msg_hdr_v2?
 type ZmqHeader struct {
 	url       string
 	version   uint8
@@ -80,9 +85,13 @@ type ZmqHeader struct {
 }
 
 func (zs *ZmqState) NewZmqHeader(length uint16) *ZmqHeader {
+	var version uint8 = ZMQ_MSG_VERSION_TLV
+	if !rctx.cli.TLV {
+		version = ZMQ_MSG_VERSION_OLD
+	}
 	z := &ZmqHeader{
 		url:       ZMQ_TOPIC,
-		version:   ZMQ_MSG_VERSION,
+		version:   version,
 		source_id: uint8(rctx.cli.SourceId),
 		length:    length,
 		msg_id:    MessageId,
@@ -262,6 +271,132 @@ func (zs *ZmqState) toJSON(flowMessage *flowmessage.FlowMessage) ([]byte, error)
 	return jdata, nil
 }
 
+/*
+ * Converts a FlowMessage to ntop's TLV format
+ */
+func (zs *ZmqState) toTLV(flowMessage *flowmessage.FlowMessage) ([]byte, error) {
+	ip6 := make(net.IP, net.IPv6len)
+	ip4 := make(net.IP, net.IPv4len)
+	hwaddr := make(net.HardwareAddr, 6)
+	_hwaddr := make([]byte, binary.MaxVarintLen64)
+	var icmp_type uint16
+
+	// Overall approach is to make an slice of NdpiItems, then serialize them all to make a
+	// record that can be published via ZMQ
+
+	// Stats + direction
+	items := []NdpiItem{{Key: netflow.NFV9_FIELD_DIRECTION, Value: flowMessage.FlowDirection}}
+	if flowMessage.FlowDirection == 0 {
+		// ingress == 0
+		items = append(items,
+			NdpiItem{Key: netflow.NFV9_FIELD_IN_BYTES, Value: flowMessage.Bytes},
+			NdpiItem{Key: netflow.NFV9_FIELD_IN_PKTS, Value: flowMessage.Packets},
+		)
+	} else {
+		// egress == 1
+		items = append(items,
+			NdpiItem{Key: netflow.NFV9_FIELD_OUT_BYTES, Value: flowMessage.Bytes},
+			NdpiItem{Key: netflow.NFV9_FIELD_OUT_PKTS, Value: flowMessage.Packets},
+		)
+	}
+	items = append(items,
+		NdpiItem{Key: netflow.NFV9_FIELD_FIRST_SWITCHED, Value: flowMessage.TimeFlowStart},
+		NdpiItem{Key: netflow.NFV9_FIELD_LAST_SWITCHED, Value: flowMessage.TimeFlowEnd},
+	)
+
+	items = append(items,
+		// L4
+		NdpiItem{Key: netflow.NFV9_FIELD_PROTOCOL, Value: flowMessage.Proto},
+		NdpiItem{Key: netflow.NFV9_FIELD_L4_SRC_PORT, Value: flowMessage.SrcPort},
+		NdpiItem{Key: netflow.NFV9_FIELD_L4_DST_PORT, Value: flowMessage.DstPort},
+		// Network
+		NdpiItem{Key: netflow.NFV9_FIELD_SRC_AS, Value: flowMessage.SrcAS},
+		NdpiItem{Key: netflow.NFV9_FIELD_DST_AS, Value: flowMessage.DstAS},
+
+		// Interfaces
+		NdpiItem{Key: netflow.NFV9_FIELD_INPUT_SNMP, Value: flowMessage.InIf},
+		NdpiItem{Key: netflow.NFV9_FIELD_OUTPUT_SNMP, Value: flowMessage.OutIf},
+		NdpiItem{Key: netflow.NFV9_FIELD_FORWARDING_STATUS, Value: flowMessage.ForwardingStatus},
+		NdpiItem{Key: netflow.NFV9_FIELD_SRC_TOS, Value: flowMessage.IPTos},
+		NdpiItem{Key: netflow.NFV9_FIELD_TCP_FLAGS, Value: flowMessage.TCPFlags},
+		NdpiItem{Key: netflow.NFV9_FIELD_MIN_TTL, Value: flowMessage.IPTTL},
+	)
+
+	// IP
+	if flowMessage.Etype == 0x800 {
+		// IPv4
+		items = append(items,
+			NdpiItem{Key: netflow.NFV9_FIELD_IP_PROTOCOL_VERSION, Value: 4},
+			NdpiItem{Key: netflow.NFV9_FIELD_IPV4_SRC_PREFIX, Value: flowMessage.SrcNet},
+			NdpiItem{Key: netflow.NFV9_FIELD_IPV4_DST_PREFIX, Value: flowMessage.DstNet},
+			NdpiItem{Key: netflow.NFV9_FIELD_IPV4_IDENT, Value: flowMessage.FragmentId},
+			NdpiItem{Key: netflow.NFV9_FIELD_FRAGMENT_OFFSET, Value: flowMessage.FragmentOffset},
+			NdpiItem{Key: netflow.NFV9_FIELD_IPV6_SRC_MASK, Value: flowMessage.SrcNet},
+			NdpiItem{Key: netflow.NFV9_FIELD_IPV6_DST_MASK, Value: flowMessage.DstNet},
+		)
+		copy(ip4, flowMessage.SrcAddr)
+		items = append(items, NdpiItem{Key: netflow.NFV9_FIELD_IPV4_SRC_ADDR, Value: ip4.String()})
+		copy(ip4, flowMessage.DstAddr)
+		items = append(items, NdpiItem{Key: netflow.NFV9_FIELD_IPV4_DST_ADDR, Value: ip4.String()})
+		copy(ip4, flowMessage.NextHop)
+		items = append(items, NdpiItem{Key: netflow.NFV9_FIELD_IPV4_NEXT_HOP, Value: ip4.String()})
+
+	} else {
+		// 0x86dd IPv6
+		items = append(items,
+			NdpiItem{Key: netflow.NFV9_FIELD_IP_PROTOCOL_VERSION, Value: 6},
+			NdpiItem{Key: netflow.NFV9_FIELD_IPV6_SRC_MASK, Value: flowMessage.SrcNet},
+			NdpiItem{Key: netflow.NFV9_FIELD_IPV6_DST_MASK, Value: flowMessage.DstNet},
+			NdpiItem{Key: netflow.NFV9_FIELD_IPV6_FLOW_LABEL, Value: flowMessage.IPv6FlowLabel},
+		)
+		copy(ip6, flowMessage.SrcAddr)
+		items = append(items, NdpiItem{Key: netflow.NFV9_FIELD_IPV6_SRC_ADDR, Value: ip6.String()})
+		copy(ip6, flowMessage.DstAddr)
+		items = append(items, NdpiItem{Key: netflow.NFV9_FIELD_IPV6_DST_ADDR, Value: ip6.String()})
+		copy(ip6, flowMessage.NextHop)
+		items = append(items, NdpiItem{Key: netflow.NFV9_FIELD_IPV6_NEXT_HOP, Value: ip6.String()})
+	}
+
+	// ICMP
+	icmp_type = uint16((uint16(flowMessage.IcmpType) << 8) + uint16(flowMessage.IcmpCode))
+	items = append(items, NdpiItem{Key: netflow.NFV9_FIELD_ICMP_TYPE, Value: icmp_type})
+
+	// MAC
+	binary.PutUvarint(_hwaddr, flowMessage.DstMac)
+	for i := 0; i < 6; i++ {
+		hwaddr[i] = _hwaddr[i]
+	}
+	items = append(items, NdpiItem{Key: netflow.NFV9_FIELD_IN_DST_MAC, Value: hwaddr.String()})
+	binary.PutUvarint(_hwaddr, flowMessage.SrcMac)
+	for i := 0; i < 6; i++ {
+		hwaddr[i] = _hwaddr[i]
+	}
+	items = append(items, NdpiItem{Key: netflow.NFV9_FIELD_OUT_SRC_MAC, Value: hwaddr.String()})
+
+	// VLAN
+	items = append(items,
+		NdpiItem{Key: netflow.NFV9_FIELD_SRC_VLAN, Value: flowMessage.SrcVlan},
+		NdpiItem{Key: netflow.NFV9_FIELD_DST_VLAN, Value: flowMessage.DstVlan},
+	)
+
+	// Flow Exporter IP
+	if len(flowMessage.SamplerAddress) == 4 {
+		copy(ip4, flowMessage.SamplerAddress)
+		items = append(items, NdpiItem{Key: netflow.IPFIX_FIELD_exporterIPv4Address, Value: ip4.String()})
+	} else if len(flowMessage.SamplerAddress) == 16 {
+		copy(ip6, flowMessage.SamplerAddress)
+		items = append(items, NdpiItem{Key: netflow.IPFIX_FIELD_exporterIPv6Address, Value: ip6.String()})
+	}
+
+	// Serialize and make a flow record.
+	tlvbuf, err := SerializeTlvRecord(items)
+	if err != nil {
+		return tlvbuf, err
+	}
+
+	return tlvbuf, nil
+}
+
 func (zs *ZmqState) Publish(msgs []*flowmessage.FlowMessage) {
 	for _, msg := range msgs {
 		zs.SendZmqMessage(msg)
@@ -274,8 +409,10 @@ func (zs *ZmqState) SendZmqMessage(flowMessage *flowmessage.FlowMessage) {
 
 	if zs.serialize == "pbuf" {
 		msg, err = proto.Marshal(flowMessage)
-	} else {
+	} else if zs.serialize == "json" {
 		msg, err = zs.toJSON(flowMessage)
+	} else {
+		msg, err = zs.toTLV(flowMessage)
 	}
 
 	if err != nil {
@@ -309,13 +446,15 @@ func (zs *ZmqState) SendZmqMessage(flowMessage *flowmessage.FlowMessage) {
 		return
 	}
 
-	if zs.serialize == "json" {
+	if zs.serialize == "pbuf" {
+		log.Debugf("sent %d bytes of pbuf:\n%s", msg_len, hex.Dump(msg))
+	} else if zs.serialize == "json" {
 		if zs.compress {
 			log.Debugf("sent %d bytes of zlib json:\n%s", msg_len, hex.Dump(msg))
 		} else {
 			log.Debugf("sent %d bytes of json: %s", msg_len, string(msg))
 		}
 	} else {
-		log.Debugf("sent %d bytes of pbuf:\n%s", msg_len, hex.Dump(msg))
+		log.Debugf("sent %d bytes of ntop tlv:\n%s", msg_len, hex.Dump(msg))
 	}
 }
